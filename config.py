4f2921fbb49420299b3a97e22dbebd015ed13602
shiamakd25 <shiamakdas@outlook.com>
2024-08-14 09:18:15 -0400

    Updated config.py to include a global variable for the tile_config df

--- config.py
+++ config.py
@@ -1,4 +1,6 @@
+import pandas as pd
+
 GRID_WIDTH = 4
 GRID_HEIGHT = 4
 TILES_NUM = 10
-# TODO: Make db for tiles to store edges as well
+TILES_DF =  pd.read_csv("tile_config.csv", dtype={'up_edge':str, 'right_edge':str, 'down_edge':str, 'left_edge':str})
